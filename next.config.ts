b003940894a1d5e1cf3b4d106c909d6f8078435a
CHIRAG-GOUR <gourchirag101@gmail.com>
2026-07-23 15:30:32 +0530

    Fix Turbopack module resolution bug with firebase-admin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@ffmpeg-installer/ffmpeg', 'firebase-admin'],
+  serverExternalPackages: ['@ffmpeg-installer/ffmpeg'],
 };
 
 export default nextConfig;
